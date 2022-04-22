c665960ff8a50942406829e09d666c075d5efa79
Alex Hankin <alexhankin@Alexs-iMac.local>
2022-04-22 14:49:26 -0400

    Model updates
    1. Every qubit has at least one 2-qubit gate
    2. Add 1-qubit and 2-qubit gate operations to list and randomize
    3. Pickle graph to promote repeatability

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import random
 import sys
 import json
 import math
+import pickle
 
 
 COLORS = ['lightsteelblue', 'red', 'lime', 'yellow', 'orange', 'navajowhite', 'plum', 'cyan', 'brown']
@@ -58,13 +59,14 @@ def sum_to_n(n, size, limit=None):
 
 def gen_random_circuit():
     
-    chain_size = 8
+    chain_size = 4
     print("Chain size: ", chain_size)
-    num_qubits = random.randint(5, 50)
+    num_qubits = random.randint(3, 16)
     print("Num qubits: ", num_qubits)
-    num_2q_gates = random.randint(0, num_qubits)
-    #num_1q_gates = random.randint(0, num_qubits)
-    num_1q_gates = 0
+    num_2q_gates = random.randint(num_qubits, num_qubits + 5)
+    print("Num 2-qubit gates: ", num_2q_gates)
+    num_1q_gates = random.randint(0, num_qubits + 10)
+    print("Num 1-qubit gates: ", num_1q_gates)
     qubit_list = list(range(num_qubits))
     print("Qubit list: ", qubit_list)
     delta = 1
@@ -73,9 +75,28 @@ def gen_random_circuit():
     return chain_size, num_qubits, num_2q_gates, num_1q_gates, qubit_list, delta, gamma, alpha
 
 
-def place_gates(num_2q_gates, qubit_list, G):
+def place_gates(num_1q_gates, num_2q_gates, qubit_list, G):
 
-    for gate in range(num_2q_gates):
+    remaining_2q_gates = num_2q_gates - len(qubit_list) # first randomly assign a 2q gate to each qubit
+
+    operation_list = []
+
+    for qubit in range(len(qubit_list)):
+        q_id = random.sample(range(0, max(qubit_list)), 1)  # sample a distinct qubit
+        q_id = q_id[0]
+        
+        while q_id == qubit:
+            q_id = random.sample(range(0, max(qubit_list)), 1)  # sample a distinct qubit
+            q_id = q_id[0]
+        if (G.has_edge('q{}'.format(qubit), 'q{}'.format(q_id))): # edge already exists; update edge weight
+            G['q{}'.format(qubit)]['q{}'.format(q_id)]['weight'] = G['q{}'.format(qubit)]['q{}'.format(q_id)]['weight'] + 1
+        else: #no edge; add edge
+            G.add_edge('q{}'.format(qubit), 'q{}'.format(q_id), weight=1)
+         
+        operation_list.append(['q{}'.format(qubit), 'q{}'.format(q_id)])
+        
+
+    for two_q_gate in range(remaining_2q_gates): # randomly assign the remaining qubits
         q_id = random.sample(range(0, max(qubit_list)), 2)  # sample 2 distinct qubits
         q1_id = q_id[0]
         q2_id = q_id[1]
@@ -84,7 +105,16 @@ def place_gates(num_2q_gates, qubit_list, G):
             G['q{}'.format(q1_id)]['q{}'.format(q2_id)]['weight'] = G['q{}'.format(q1_id)]['q{}'.format(q2_id)]['weight'] + 1
         else: #no edge; add edge
             G.add_edge('q{}'.format(q1_id), 'q{}'.format(q2_id), weight=1)
-    return G
+        
+        operation_list.append(['q{}'.format(q1_id), 'q{}'.format(q2_id)]) 
+
+    for one_q_gate in range(num_1q_gates):
+        q_id = random.sample(range(0, max(qubit_list)), 1)  # sample a distinct qubit
+        q_id = q_id[0]
+        operation_list.append(['q{}'.format(q_id)])
+       
+
+    return G, operation_list
 
 
 def draw_graph(G, indices):
@@ -108,20 +138,21 @@ def draw_graph(G, indices):
     plt.savefig('my_graph.png')
 
 
-def get_indices(idx, partitions):
+def get_indices(partitions):
+    indices = []
+    index = -1
+    first = 1
+    for partition_size in partitions:
+        if first:
+            indices.append(partition_size)
+            index = partition_size
+            first = 0
+        else:
+            indices.append(index + partition_size)
+            index = index + partition_size
 
-    # get the indices
-    indices = partitions[idx]
-    indices.sort()
-    indices_set = set(indices)
-    
-    # check if duplicates and regen until there aren't
-    while (len(indices) != len(indices_set)):
-        # choose one of those ways randomly
-        idx = random.randint(0, len(partitions)-1)
-        indices = partitions[idx]
-        indices.sort()
-        indices_set = set(indices)
+    indices.pop()
+    print("Indices: ", indices)
 
     return indices
 
@@ -152,48 +183,50 @@ def compute_num_weak_links(num_chains, G, prepped_subgroups):
     return cut_sizes
 
 
-def get_placement_possibilities(qubit_list, num_chains, num_qubits, G):
+def get_placement_possibilities(chain_size, qubit_list, num_chains, num_qubits, G):
     
     # get all ways to break up the graph into subgraphs 
-    partitions = list(sum_to_n(len(qubit_list)-1, num_chains-1))
+    partition_list = list(sum_to_n(len(qubit_list)-1, num_chains))
+    
+    print("Partitions: ", partition_list)
     
     # choose one of those ways randomly
-    idx = random.randint(0, len(partitions)-1)
+    idx = random.randint(0, len(partition_list)-1)
+
+    while any(partition > chain_size for partition in partition_list[idx]):
+        # choose one of those ways randomly
+        idx = random.randint(0, len(partition_list)-1)
     
     # get the indices (no duplicates)
-    indices = get_indices(idx, partitions)
-    
-    print("Indices: ")
-    print(indices)
+    indices = get_indices(partition_list[idx])
     
     subgroups = []
     
-    first = 1
-    for ind in indices:
-       if first == 1: 
-           subgroups.append(list(range(0, ind))) 
-           old_ind = ind
-           first = 0
-       else:
-           subgroups.append(list(range(old_ind, ind)))
-           old_ind = ind
+    if len(indices) != 0:
+        first = 1
+        for ind in indices:
+           if first == 1: 
+               subgroups.append(list(range(0, ind))) 
+               old_ind = ind
+               first = 0
+           else:
+               subgroups.append(list(range(old_ind, ind)))
+               old_ind = ind
+        
+        subgroups.append(list(range(indices[len(indices)-1], num_qubits)))
+    else:
+        subgroups.append(list(range(0, num_qubits)))
     
-    subgroups.append(list(range(indices[len(indices)-1], num_qubits)))
-    
-    print("Subgroups: ")
-    print(subgroups)
-
     # prep subgroups (e.g., append q to qubit id's)
     prepped_subgroups = prep_subgroups(subgroups)
     
-    print("Prepped subgroups: ")
+    print("Subgroups: ")
     print(prepped_subgroups)
-    print()
        
     # compute number of weak links (cut sizes between chains)
     cut_sizes = compute_num_weak_links(num_chains, G, prepped_subgroups)
 
-    return cut_sizes, indices
+    return prepped_subgroups, cut_sizes, indices
 
 
 def place_qubits_into_chains(G, num_qubits, chain_size, qubit_list):
@@ -206,13 +239,17 @@ def place_qubits_into_chains(G, num_qubits, chain_size, qubit_list):
     random.shuffle(qubit_list)
     
     print("PLACEMENT ATTEMPT")
-    cut_sizes, indices = get_placement_possibilities(qubit_list, num_chains, num_qubits, G)
+    prepped_subgroups, cut_sizes, indices = get_placement_possibilities(chain_size, qubit_list, num_chains, num_qubits, G)
+    print("Cut sizes: ", cut_sizes)
+    print()
     
     while not all(i <= 2 for i in cut_sizes):
         print("PLACEMENT ATTEMPT")
-        cut_sizes, indices = get_placement_possibilities(qubit_list, num_chains, num_qubits, G)
+        prepped_subgroups, cut_sizes, indices = get_placement_possibilities(chain_size, qubit_list, num_chains, num_qubits, G)
+        print("Cut sizes: ", cut_sizes)
+        print()
 
-    return cut_sizes, indices
+    return prepped_subgroups, cut_sizes, indices
 
 
 def main():
@@ -221,9 +258,10 @@ def main():
         print("GENERATING RANDOM CIRCUIT")
         chain_size, num_qubits, num_2q_gates, num_1q_gates, qubit_list, delta, gamma, alpha = gen_random_circuit()
     else: # load circuit from config
-        d, q, p, alpha, delta, gamma = load_config("./configs/3layer_test.json")
+        d, num_1q_gates, num_2q_gates, alpha, delta, gamma = load_config("./configs/3layer_test.json")
     
     G = nx.Graph()
+    #G = pickle.load(open('./saved_graphs/graph.pkl'))
     
     # initialize qubits (add nodes)
     for qubit_num in qubit_list:
@@ -232,25 +270,36 @@ def main():
     print("Nodes: ", G.nodes())
     
     # place gates (add edges)
-    G = place_gates(num_2q_gates, qubit_list, G)
+    G, operation_list = place_gates(num_1q_gates, num_2q_gates, qubit_list, G)
     
     print("Edges: ", G.edges())
 
-   #Place qubits into chains
+    #Place qubits into chains
     # number of sequences is equal to number of chains
     # break qubit list into X random groups where X is equal to number of chains
-    cut_sizes, indices = place_qubits_into_chains(G, num_qubits, chain_size, qubit_list)
+    prepped_subgroups, cut_sizes, indices = place_qubits_into_chains(G, num_qubits, chain_size, qubit_list)
 
     # draw graph for visualization purposes
     draw_graph(G, indices)
 
+    pickle.dump(G, open('./saved_graphs/graph.pkl', 'wb'))
+
     print("RESULTS")
+    print("Subgroups: ")
+    print(prepped_subgroups)
     print("Cut sizes: ", cut_sizes)
     
-    t = delta*num_1q_gates # 1 qubit-gate latencies
-    t = t + (num_2q_gates-sum(cut_sizes))*gamma + sum(cut_sizes)*gamma*alpha # 2-qubit gate latencies
+    serial_t = delta*num_1q_gates # 1 qubit-gate latencies
+    serial_t = serial_t + (num_2q_gates-sum(cut_sizes))*gamma + sum(cut_sizes)*gamma*alpha # 2-qubit gate latencies
     
-    print("Total time [us]: ", t)
+    print("Serial performance [us]: ", serial_t)
+
+    print("Operation list: ", operation_list)
+    random.shuffle(operation_list) # randomize order of operations
+    print("Operation list: ", operation_list)
+    
+    #parallel_t = new_algorithm(operation_list)
+    #print("Parallel performance [us]: ", parallel_t)
 
 
 if __name__ == "__main__":
